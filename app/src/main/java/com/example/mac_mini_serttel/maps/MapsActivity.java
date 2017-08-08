ca3d6b78648cdae37bd0d442459cfac6b139c792
mac-mini-serttel <rmso@cin.ufpe.br>
2017-08-08 14:27:25 -0300

    Marcação de lugar com clique longo

--- app/src/main/java/com/example/mac_mini_serttel/maps/MapsActivity.java
+++ app/src/main/java/com/example/mac_mini_serttel/maps/MapsActivity.java
@@ -1,10 +1,15 @@
 package com.example.mac_mini_serttel.maps;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -26,15 +31,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, LocationListener {
 
     private GoogleMap mMap;
-    static ArrayList<String> lugares;
-    static ArrayAdapter arrayAdapter;
     static ArrayList<LatLng> localizacoes;
 
-
+    LocationManager locationManager;
+    String provider;
+    int localizacao = -1;
 
     String[] permissoes = new String[]{
             Manifest.permission.ACCESS_FINE_LOCATION,
@@ -48,10 +54,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         PermissionUtils.validate(this, 0, permissoes);
 
+        localizacoes = new ArrayList<>();
+        localizacoes.add(new LatLng(0 , 0));
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        provider = locationManager.getBestProvider(new Criteria(), false);
 
 
     }
@@ -60,12 +72,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
+        //Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         mMap.setOnMapLongClickListener(this);
+
+        if (localizacao != -1 && localizacao != 0){
+            locationManager.removeUpdates(this);
+
+            mMap.addMarker(new MarkerOptions().position(MapsActivity.localizacoes.get(localizacao)).title("aqui"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(MapsActivity.localizacoes.get(localizacao), 10));
+
+        }else {
+            locationManager.requestLocationUpdates(provider, 400, 1, this);
+        }
     }
 
     @Override
@@ -110,8 +132,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         MapsActivity.localizacoes.add(point);
-        MapsActivity.lugares.add(marcador);
-        MapsActivity.arrayAdapter.notifyDataSetChanged();
 
         mMap.addMarker(new MarkerOptions()
                 .position(point)
@@ -120,4 +140,23 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    @Override
+    public void onLocationChanged(Location localizacaoUsuario) {
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(localizacaoUsuario.getLatitude(), localizacaoUsuario.getLongitude()), 10));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
